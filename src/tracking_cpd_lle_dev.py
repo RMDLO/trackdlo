9e86b5ce46506cfaab64b6986cca0cc1cf2fd002
Jingyi Xiang <zgzx618@gmail.com>
2022-07-25 18:32:48 -0500

    switched to hsv for color thresholding

--- src/tracking_cpd_lle_dev.py
+++ src/tracking_cpd_lle_dev.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import matplotlib.pyplot as plt
 import rospy
 import ros_numpy
 from sensor_msgs.msg import PointCloud2, PointField, Image
@@ -84,11 +85,15 @@ def register(pts, M, mu=0, max_iter=10):
 # k -- going left for k indices, going right for k indices. a total of 2k neighbors.
 def get_nearest_indices (k, Y, idx):
     if idx - k < 0:
+        # use more neighbors from the other side?
         indices_arr = np.append(np.arange(0, idx, 1), np.arange(idx+1, idx+k+1+np.abs(idx-k)))
+        # indices_arr = np.append(np.arange(0, idx, 1), np.arange(idx+1, idx+k+1))
         return indices_arr
     elif idx + k >= len(Y):
         last_index = len(Y) - 1
+        # use more neighbots from the other side?
         indices_arr = np.append(np.arange(idx-k-(idx+k-last_index), idx, 1), np.arange(idx+1, last_index+1, 1))
+        # indices_arr = np.append(np.arange(idx-k, idx, 1), np.arange(idx+1, last_index+1, 1))
         return indices_arr
     else:
         indices_arr = np.append(np.arange(idx-k, idx, 1), np.arange(idx+1, idx+k+1, 1))
@@ -99,7 +104,7 @@ def calc_LLE_weights (k, X):
     for i in range (0, len(X)):
         indices = get_nearest_indices(int(k/2), X, i)
         xi, Xi = X[i], X[indices, :]
-        component = np.full((k, len(xi)), xi).T - Xi.T
+        component = np.full((len(Xi), len(xi)), xi).T - Xi.T
         Gi = np.matmul(component.T, component)
         # Gi might be singular when k is large
         try:
@@ -107,13 +112,13 @@ def calc_LLE_weights (k, X):
         except:
             epsilon = 0.00001
             Gi_inv = np.linalg.inv(Gi + epsilon*np.identity(len(Gi)))
-        wi = np.matmul(Gi_inv, np.ones((k, 1))) / np.matmul(np.matmul(np.ones(k,), Gi_inv), np.ones((k, 1)))
+        wi = np.matmul(Gi_inv, np.ones((len(Xi), 1))) / np.matmul(np.matmul(np.ones(len(Xi),), Gi_inv), np.ones((len(Xi), 1)))
         W[i, indices] = np.squeeze(wi.T)
 
     return W
 
-def cpd (X, Y_0, beta, alpha, k, gamma, mu, max_iter, tol):
-# def cpd (X, Y_0, beta, alpha, H, gamma, mu, max_iter, tol):
+def cpd_lle (X, Y_0, beta, alpha, k, gamma, mu, max_iter, tol):
+# def cpd_lle (X, Y_0, beta, alpha, H, gamma, mu, max_iter, tol):
 
     # define params
     M = len(Y_0)
@@ -211,26 +216,13 @@ def find_opposite_closest (pt, arr, direction_pt):
         vec1 = np.array(cur_closest) - np.array(pt)
         vec2 = np.array(direction_pt) - np.array(pt)
 
-        if np.dot (vec1, vec2) < 0:
+        # threshold: 0.05m
+        if (np.dot (vec1, vec2) < 0) and (pt2pt_dis_sq(np.array(cur_closest), np.array(pt)) < 0.05**2):
             opposite_closest_found = True
             opposite_closest = cur_closest.copy()
             break
     
     return opposite_closest, opposite_closest_found
-
-def find_aligned_closest (pt, arr, direction_pt):
-    arr_copy = arr.copy()
-    aligned_closest_found = False
-    aligned_closest = pt.copy()  # will get overwritten
-
-    while (not aligned_closest_found) and (len(arr_copy) != 0):
-        cur_closest, cur_index = find_closest (pt, arr_copy)
-        arr_copy.pop (cur_index)
-        if np.dot (np.array(direction_pt), np.array(cur_closest)) > 0:
-            aligned_closest_found = True
-            aligned_closest = cur_closest.copy()
-    
-    return aligned_closest, aligned_closest_found
 
 def sort_pts (pts_orig):
 
@@ -300,12 +292,14 @@ initialized = False
 init_nodes = []
 nodes = []
 # H = []
+cur_time = time.time()
 def callback (rgb, depth, pc):
     global saved
     global initialized
     global init_nodes
     global nodes
     # global H
+    global cur_time
 
     proj_matrix = np.array([[918.359130859375,              0.0, 645.8908081054688, 0.0], \
                             [             0.0, 916.265869140625,   354.02392578125, 0.0], \
@@ -314,6 +308,12 @@ def callback (rgb, depth, pc):
     # process rgb image
     cur_image = ros_numpy.numpify(rgb)
     # cur_image = cv2.cvtColor(cur_image.copy(), cv2.COLOR_BGR2RGB)
+    hsv_image = cv2.cvtColor(cur_image.copy(), cv2.COLOR_RGB2HSV)
+
+    # # test
+    # cv2.imshow('img', cur_image)
+    # cv2.waitKey(0) 
+    # cv2.destroyAllWindows()
 
     # process depth image
     cur_depth = ros_numpy.numpify(depth)
@@ -323,14 +323,17 @@ def callback (rgb, depth, pc):
     cur_pc = ros_numpy.point_cloud2.get_xyz_points(pc_data)
     cur_pc = cur_pc.reshape((720, 1280, 3))
 
-    # test
-    # print(np.shape(cur_image), np.shape(cur_depth), np.shape(cur_pc))
-
     # color thresholding
-    lower = (40, 40, 135)
-    upper = (125, 125, 255)
-    mask = cv2.inRange(cur_image, lower, upper)
+    lower = (90, 100, 100)
+    upper = (120, 255, 255)
+    mask = cv2.inRange(hsv_image, lower, upper)
     mask = cv2.cvtColor(mask.copy(), cv2.COLOR_GRAY2BGR)
+    print(np.shape(mask))
+
+    # publish mask
+    mask_img_msg = ros_numpy.msgify(Image, mask, 'rgb8')
+    mask_img_pub.publish(mask_img_msg)
+
     mask = (mask/255).astype(int)
 
     filtered_pc = cur_pc*mask
@@ -358,7 +361,7 @@ def callback (rgb, depth, pc):
     print(np.shape(filtered_pc))
 
     # add color
-    pc_rgba = struct.unpack('I', struct.pack('BBBB', 255, 0, 0, 255))[0]
+    pc_rgba = struct.unpack('I', struct.pack('BBBB', 255, 40, 40, 255))[0]
     pc_rgba_arr = np.full((len(filtered_pc), 1), pc_rgba)
     filtered_pc_colored = np.hstack((filtered_pc, pc_rgba_arr)).astype('O')
     filtered_pc_colored[:, 3] = filtered_pc_colored[:, 3].astype(int)
@@ -382,8 +385,8 @@ def callback (rgb, depth, pc):
 
     # cpd
     if initialized:
-        nodes = cpd(X=filtered_pc, Y_0 = init_nodes, beta=2, alpha=1, k=6, gamma=3, mu=0.05, max_iter=30, tol=0.00001)
-        # nodes = cpd(X=filtered_pc, Y_0 = init_nodes, beta=1, alpha=1, H=H, gamma=2, mu=0.05, max_iter=30, tol=0.00001)
+        nodes = cpd_lle(X=filtered_pc, Y_0 = init_nodes, beta=2, alpha=1, k=6, gamma=3, mu=0.05, max_iter=30, tol=0.00001)
+        # nodes = cpd_lle(X=filtered_pc, Y_0 = init_nodes, beta=1, alpha=1, H=H, gamma=2, mu=0.05, max_iter=30, tol=0.00001)
         init_nodes = nodes
         print("finished reg")
 
@@ -416,6 +419,9 @@ def callback (rgb, depth, pc):
         tracking_img_msg = ros_numpy.msgify(Image, tracking_img, 'rgb8')
         tracking_img_pub.publish(tracking_img_msg)
 
+        print(time.time() - cur_time)
+        cur_time = time.time()
+
 
 if __name__=='__main__':
     rospy.init_node('test', anonymous=True)
@@ -436,6 +442,7 @@ if __name__=='__main__':
     init_nodes_pub = rospy.Publisher ('/init_nodes', PointCloud2, queue_size=10)
     nodes_pub = rospy.Publisher ('/nodes', PointCloud2, queue_size=10)
     tracking_img_pub = rospy.Publisher ('/tracking_img', Image, queue_size=10)
+    mask_img_pub = rospy.Publisher('/mask', Image, queue_size=10)
 
     ts = message_filters.TimeSynchronizer([rgb_sub, depth_sub, pc_sub], 10)
     ts.registerCallback(callback)
