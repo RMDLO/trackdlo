0b64ede7610ff752e00041f576b296e3a44799c0
Jingyi Xiang <zgzx618@gmail.com>
2022-11-20 18:30:34 -0600

    finished basic cpd implementation in c++

--- src/cpp/test.cpp
+++ src/cpp/test.cpp
@@ -47,9 +47,6 @@ MatrixXd calc_LLE_weights (int k, MatrixXd X) {
     for (int i = 0; i < X.rows(); i ++) {
         std::vector<int> indices = get_nearest_indices(static_cast<int>(k/2), X.rows(), i);
         MatrixXd xi = X.row(i);
-        // std::cout << "--- xi ---" << std::endl;
-        // std::cout << xi << std::endl;
-
         MatrixXd Xi = MatrixXd(indices.size(), X.cols());
 
         // fill in Xi: Xi = X[indices, :]
@@ -57,16 +54,9 @@ MatrixXd calc_LLE_weights (int k, MatrixXd X) {
             Xi.row(r) = X.row(indices[r]);
         }
 
-        // std::cout << "--- Xi ---" << std::endl;
-        // std::cout << Xi << std::endl;
-
         // component = np.full((len(Xi), len(xi)), xi).T - Xi.T
         MatrixXd component = xi.replicate(Xi.rows(), 1).transpose() - Xi.transpose();
-        
         MatrixXd Gi = component.transpose() * component;
-        // std::cout << "--- Gi ---" << std::endl;
-        // std::cout << Gi << std::endl;
-        
         MatrixXd Gi_inv;
 
         if (Gi.determinant() != 0) {
@@ -79,20 +69,12 @@ MatrixXd calc_LLE_weights (int k, MatrixXd X) {
             Gi_inv = Gi.inverse();
         }
 
-        // std::cout << "--- Gi_inv ---" << std::endl;
-        // std::cout << Gi_inv << std::endl;
-
         // wi = Gi_inv * 1 / (1^T * Gi_inv * 1)
         MatrixXd ones_row_vec = MatrixXd::Constant(1, Xi.rows(), 1.0);
         MatrixXd ones_col_vec = MatrixXd::Constant(Xi.rows(), 1, 1.0);
 
         MatrixXd wi = (Gi_inv * ones_col_vec) / (ones_row_vec * Gi_inv * ones_col_vec).value();
-        // std::cout << "--- wi ---" << std::endl;
-        // std::cout << wi << std::endl;
-        
         MatrixXd wi_T = wi.transpose();
-        // std::cout << "--- wi_T ---" << std::endl;
-        // std::cout << wi_T << std::endl;
 
         for (int c = 0; c < indices.size(); c ++) {
             W(i, indices[c]) = wi_T(c);
@@ -110,6 +92,10 @@ void print_1d_vector (std::vector<T> vec) {
     std::cout << std::endl;
 }
 
+double pt2pt_dis_sq (MatrixXd pt1, MatrixXd pt2) {
+    return (pt1 - pt2).rowwise().squaredNorm().sum();
+}
+
 MatrixXd cpd (MatrixXd X_orig,
               MatrixXd Y_0,
               double beta,
@@ -118,6 +104,7 @@ MatrixXd cpd (MatrixXd X_orig,
               double mu,
               int max_iter = 30,
               double tol = 0.00001,
+              bool include_lle = true,
               bool use_geodesic = false,
               bool use_prev_sigma2 = false,
               double sigma2_0 = 0,
@@ -127,6 +114,9 @@ MatrixXd cpd (MatrixXd X_orig,
               std::string kernel = "Gaussian",
               std::vector<int> occluded_nodes = {}) {
 
+    // log time            
+    clock_t cur_time = clock();
+
     MatrixXd X = X_orig.replicate(1, 1);
 
     int M = Y_0.rows();
@@ -135,12 +125,20 @@ MatrixXd cpd (MatrixXd X_orig,
 
     std::cout << "---" << std::endl;
 
-    MatrixXd diff = MatrixXd::Zero(M, M);
-    MatrixXd diff_sqrt = MatrixXd::Zero(M, M);
-    for (int i = 0; i < Y_0.rows(); i ++) {
-        for (int j = 0; j < Y_0.rows(); j ++) {
-            diff(i, j) = (Y_0.row(i) - Y_0.row(j)).squaredNorm();
-            diff_sqrt(i, j) = (Y_0.row(i) - Y_0.row(j)).norm();
+    MatrixXd diff_yy = MatrixXd::Zero(M, M);
+    MatrixXd diff_yy_sqrt = MatrixXd::Zero(M, M);
+    for (int i = 0; i < M; i ++) {
+        for (int j = 0; j < M; j ++) {
+            diff_yy(i, j) = (Y_0.row(i) - Y_0.row(j)).squaredNorm();
+            diff_yy_sqrt(i, j) = (Y_0.row(i) - Y_0.row(j)).norm();
+        }
+    }
+
+    // diff_xy should be a (M * N) matrix
+    MatrixXd diff_xy = MatrixXd::Zero(M, N);
+    for (int i = 0; i < M; i ++) {
+        for (int j = 0; j < N; j ++) {
+            diff_xy(i, j) = (Y_0.row(i) - X.row(j)).squaredNorm();
         }
     }
 
@@ -149,19 +147,19 @@ MatrixXd cpd (MatrixXd X_orig,
     MatrixXd G = MatrixXd::Zero(M, M);
     if (!use_geodesic) {
         if (kernel == "Gaussian") {
-            G = (-diff / (2 * beta * beta)).array().exp();
+            G = (-diff_yy / (2 * beta * beta)).array().exp();
         }
         else if (kernel == "Laplacian") {
-            G = (-diff_sqrt / (2 * beta * beta)).array().exp();
+            G = (-diff_yy_sqrt / (2 * beta * beta)).array().exp();
         }
         else if (kernel == "1st order") {
-            G = 1/(2*beta * 2*beta) * (-sqrt(2)*diff_sqrt/beta).array().exp() * (sqrt(2)*diff_sqrt.array() + beta);
+            G = 1/(2*beta * 2*beta) * (-sqrt(2)*diff_yy_sqrt/beta).array().exp() * (sqrt(2)*diff_yy_sqrt.array() + beta);
         }
         else if (kernel == "2nd order") {
-            G = 27 * 1/(72 * pow(beta, 3)) * (-sqrt(3)*diff_sqrt/beta).array().exp() * (sqrt(3)*beta*beta + 3*beta*diff_sqrt.array() + sqrt(3)*diff.array());
+            G = 27 * 1/(72 * pow(beta, 3)) * (-sqrt(3)*diff_yy_sqrt/beta).array().exp() * (sqrt(3)*beta*beta + 3*beta*diff_yy_sqrt.array() + sqrt(3)*diff_yy.array());
         }
         else { // default to gaussian
-            G = (-diff / (2 * beta * beta)).array().exp();
+            G = (-diff_yy / (2 * beta * beta)).array().exp();
         }
     }
     else {
@@ -189,14 +187,114 @@ MatrixXd cpd (MatrixXd X_orig,
             G = 1/(2*beta * 2*beta) * (-sqrt(2)*converted_node_dis/beta).array().exp() * (sqrt(2)*converted_node_dis.array() + beta);
         }
         else if (kernel == "2nd order") {
-            G = 27 * 1/(72 * pow(beta, 3)) * (-sqrt(3)*converted_node_dis/beta).array().exp() * (sqrt(3)*beta*beta + 3*beta*converted_node_dis.array() + sqrt(3)*diff.array());
+            G = 27 * 1/(72 * pow(beta, 3)) * (-sqrt(3)*converted_node_dis/beta).array().exp() * (sqrt(3)*beta*beta + 3*beta*converted_node_dis.array() + sqrt(3)*diff_yy.array());
         }
         else { // default to gaussian
             G = (-converted_node_dis_sq / (2 * beta * beta)).array().exp();
         }
     }
+
+    MatrixXd Y = Y_0.replicate(1, 1);
+
+    // initialize sigma2
+    double sigma2 = 0;
+    if (!use_prev_sigma2) {
+        sigma2 = diff_xy.sum() / (static_cast<double>(D * M * N));
+    }
+    else {
+        sigma2 = sigma2_0;
+    }
+
+    // get the LLE matrix
+    MatrixXd L = calc_LLE_weights(2, Y_0);
+    MatrixXd H = (MatrixXd::Identity(M, M) - L).transpose() * (MatrixXd::Identity(M, M) - L);
+
+    // TODO: implement node deletion from the original point cloud
+
+    for (int it = 0; it < max_iter; it ++) {
+
+        // update diff_xy
+        for (int i = 0; i < M; i ++) {
+            for (int j = 0; j < N; j ++) {
+                diff_xy(i, j) = (Y_0.row(i) - X.row(j)).squaredNorm();
+            }
+        }
+
+        MatrixXd P = (-0.5 * diff_xy / sigma2).array().exp();
+        double c = pow((2 * M_PI * sigma2), static_cast<double>(D)/2) * mu / (1 - mu) * static_cast<double>(M)/N;
+        P = P.array().rowwise() / (P.colwise().sum().array() + c);
+
+        std::vector<int> max_p_nodes(P.cols(), 0);
+        for (int i = 0; i < N; i ++) {
+            P.col(i).maxCoeff(&max_p_nodes[i]);
+        }
+
+        // TODO: implement geodesic dists
+        // if (use_geodesic) {
+        //     std::vector<int> potential_2nd_max_p_nodes_1(P.cols(), 0);   // -1
+        //     std::vector<int> potential_2nd_max_p_nodes_2(P.cols(), 0);   // +1
+        //     for (int i = 0; i < N; i ++) {
+        //         // potential_2nd_max_p_nodes_1 = max_p_nodes - 1
+        //         if (max_p_nodes[i] == 0) {
+        //             potential_2nd_max_p_nodes_1[i] = 1;
+        //         }
+        //         else {
+        //             potential_2nd_max_p_nodes_1[i] = max_p_nodes[i] - 1;
+        //         }
+
+        //         // potential_2nd_max_p_nodes_2 = max_p_nodes + 1
+        //         if (max_p_nodes[i] == M - 1) {
+        //             potential_2nd_max_p_nodes_2[i] = M - 2;
+        //         }
+        //         else {
+        //             potential_2nd_max_p_nodes_2[i] = max_p_nodes[i] + 1;
+        //         }
+        //     }
+        // }
+
+        MatrixXd Pt1 = P.colwise().sum();
+        MatrixXd P1 = P.rowwise().sum();
+        double Np = P1.sum();
+        MatrixXd PX = P * X;
+
+        // M step
+        MatrixXd A_matrix;
+        MatrixXd B_matrix;
+        if (include_lle) {
+            A_matrix = P1.asDiagonal() * G + alpha * sigma2 * MatrixXd::Identity(M, M) + sigma2 * gamma * H * G;
+            B_matrix = PX - P1.asDiagonal() * Y_0 - sigma2 * gamma * H * Y_0;
+        }
+        else {
+            A_matrix = P1.asDiagonal() * G + alpha * sigma2 * MatrixXd::Identity(M, M);
+            B_matrix = PX - P1.asDiagonal() * Y_0;
+        }
+
+        MatrixXd W = (A_matrix).householderQr().solve(B_matrix);
+
+        MatrixXd T = Y_0 + G * W;
+        double trXtdPt1X = (X.transpose() * Pt1.asDiagonal() * X).trace();
+        double trPXtT = (PX.transpose() * T).trace();
+        double trTtdP1T = (T.transpose() * P1.asDiagonal() * T).trace();
+
+        sigma2 = (trXtdPt1X - 2*trPXtT + trTtdP1T) / (Np * D);
+
+        if (pt2pt_dis_sq(Y, Y_0 + G*W) < tol) {
+            Y = Y_0 + G*W;
+            std::cout << "iterations until convergence: " << it << std::endl;
+            break;
+        }
+        else {
+            Y = Y_0 + G*W;
+        }
+
+        if (it == max_iter - 1) {
+            std::cout << "optimization did not converge!" << std::endl;
+            break;
+        }
+    }
     
-    return G;
+    std::cout << "time taken:" << static_cast<double>(clock() - cur_time) / static_cast<double>(CLOCKS_PER_SEC) << std::endl;
+    return Y;
 }
 
 int main(int argc, char **argv) {
@@ -207,17 +305,33 @@ int main(int argc, char **argv) {
     for (int i = 0; i < m1.rows(); i ++) {
         for (int j = 0; j < m1.cols(); j ++) {
             m1(i, j) = (static_cast<float>(i)*m1.cols() + static_cast<float>(j))/100;
-            m1(i, j) *= m1(i, j);
+            // m1(i, j) *= m1(i, j);
         }
     }
 
+    MatrixXd m2 = MatrixXd::Zero(10, 3);
+    for (int i = 0; i < m2.rows(); i ++) {
+        for (int j = 0; j < m2.cols(); j ++) {
+            m2(i, j) = (static_cast<float>(i)*m2.cols() + static_cast<float>(j))/200;
+            // m1(i, j) *= m1(i, j);
+        }
+    }
+
+    std::cout << m1 << std::endl;
+    std::cout << m2 << std::endl;
+
     // // ----- test LLE weights -----
-    // std::cout << m1 << std::endl;
     // clock_t cur_time = clock();
     // MatrixXd out = calc_LLE_weights(2, m1);
     // std::cout << static_cast<double>(clock() - cur_time) / static_cast<double>(CLOCKS_PER_SEC) << std::endl;
     // // std::cout << out << std::endl;
 
+    // // ----- test pt2pt_dis_sq -----
+    // MatrixXd m3 = m2.array() + 0.5;
+    // std::cout << m3 << std::endl;
+    // std::cout << pt2pt_dis_sq(m2, m3) << std::endl;
+
     // ----- test ecpd -----
-    std::cout << cpd(MatrixXd::Zero(1, 3), m1, 0.3, 0, 0, 0, 30, 0.00001, false, false, 0, false, MatrixXd(0, 0), 0, "Gaussian") << std::endl;
+    std::cout << cpd(m2, m1, 0.3, 1, 1, 0.1, 30, 0.00001, true, false, false, 0, false, MatrixXd(0, 0), 0, "Gaussian") << std::endl;
+    // print_1d_vector(cpd(m2, m1, 0.3, 1, 1, 0.05, 1, 0.00001, false, false, 0, false, MatrixXd(0, 0), 0, "Gaussian"));
 }
--- src/utils/check_cpp.py
+++ src/utils/check_cpp.py
@@ -99,6 +99,9 @@ def ecpd_lle (X_orig,                      # input point cloud
               kernel = 'Gaussian',          # Gaussian, Laplacian, 1st order, 2nd order
               occluded_nodes = None):       # nodes that are not in this array are either head nodes or tail nodes
 
+    # log time
+    cur_time = time.time()
+
     X = X_orig.copy()
 
     # define params
@@ -146,8 +149,6 @@ def ecpd_lle (X_orig,                      # input point cloud
             G = 27 * 1/(72*beta**3) * np.exp(-math.sqrt(3)*converted_node_dis/beta) * (np.sqrt(3)*beta**2 + 3*beta*converted_node_dis + np.sqrt(3)*converted_node_dis_sq)
         else:
             G = np.exp(-converted_node_dis_sq / (2 * beta**2))
-
-    return G
     
     Y = Y_0.copy()
 
@@ -158,37 +159,38 @@ def ecpd_lle (X_orig,                      # input point cloud
         diff = X[None, :, :] - Y[:, None, :]
         err = diff ** 2
         sigma2 = np.sum(err) / (D * M * N)
+        print(D, M, N)
     else:
         sigma2 = sigma2_0
 
     # get the LLE matrix
-    L = calc_LLE_weights(6, Y_0)
+    L = calc_LLE_weights(2, Y_0)
     H = np.matmul((np.identity(M) - L).T, np.identity(M) - L)
 
-    # TEMP TEST
-    if (occluded_nodes is not None) and (len(occluded_nodes) != 0):
-        pts_dis_sq = np.sum((X[None, :, :] - Y[:, None, :]) ** 2, axis=2)
-        c = (2 * np.pi * sigma2) ** (D / 2)
-        c = c * mu / (1 - mu)
-        c = c * M / N
-        P = np.exp(-pts_dis_sq / (2 * sigma2))
-        den = np.sum(P, axis=0)
-        den = np.tile(den, (M, 1))
-        den[den == 0] = np.finfo(float).eps
-        den += c
-        P = np.divide(P, den)
-        max_p_nodes = np.argmax(P, axis=0)
+    # # TEMP TEST
+    # if (occluded_nodes is not None) and (len(occluded_nodes) != 0):
+    #     pts_dis_sq = np.sum((X[None, :, :] - Y[:, None, :]) ** 2, axis=2)
+    #     c = (2 * np.pi * sigma2) ** (D / 2)
+    #     c = c * mu / (1 - mu)
+    #     c = c * M / N
+    #     P = np.exp(-pts_dis_sq / (2 * sigma2))
+    #     den = np.sum(P, axis=0)
+    #     den = np.tile(den, (M, 1))
+    #     den[den == 0] = np.finfo(float).eps
+    #     den += c
+    #     P = np.divide(P, den)
+    #     max_p_nodes = np.argmax(P, axis=0)
 
-        # determine the indices where head, tail, floating region starts/ends
-        M_head = occluded_nodes[0]
-        M_tail = M - 1 - occluded_nodes[-1]
+    #     # determine the indices where head, tail, floating region starts/ends
+    #     M_head = occluded_nodes[0]
+    #     M_tail = M - 1 - occluded_nodes[-1]
 
-        # critical nodes: M_head and M-M_tail-1
-        X = np.delete(X, (max_p_nodes == M_head)|(max_p_nodes == M-M_tail-1), 0)
+    #     # critical nodes: M_head and M-M_tail-1
+    #     X = np.delete(X, (max_p_nodes == M_head)|(max_p_nodes == M-M_tail-1), 0)
 
-    if correspondence_priors is not None and len(correspondence_priors) != 0:
-        additional_pc = correspondence_priors[:, 1:4]
-        X = np.vstack((additional_pc, X))
+    # if correspondence_priors is not None and len(correspondence_priors) != 0:
+    #     additional_pc = correspondence_priors[:, 1:4]
+    #     X = np.vstack((additional_pc, X))
 
     # add color
     pc_rgba = struct.unpack('I', struct.pack('BBBB', 255, 40, 40, 255))[0]
@@ -206,7 +208,7 @@ def ecpd_lle (X_orig,                      # input point cloud
         c = (2 * np.pi * sigma2) ** (D / 2)
         c = c * mu / (1 - mu)
         c = c * M / N
-        P = np.exp(-pts_dis_sq / (2 * sigma2))
+        P = np.exp(-pts_dis_sq / (2 * sigma2))        
         den = np.sum(P, axis=0)
         den = np.tile(den, (M, 1))
         den[den == 0] = np.finfo(float).eps
@@ -214,93 +216,93 @@ def ecpd_lle (X_orig,                      # input point cloud
         P = np.divide(P, den)
         max_p_nodes = np.argmax(P, axis=0)
 
-        if use_geodesic:
-            potential_2nd_max_p_nodes_1 = max_p_nodes - 1
-            potential_2nd_max_p_nodes_2 = max_p_nodes + 1
-            potential_2nd_max_p_nodes_1 = np.where(potential_2nd_max_p_nodes_1 < 0, 1, potential_2nd_max_p_nodes_1)
-            potential_2nd_max_p_nodes_2 = np.where(potential_2nd_max_p_nodes_2 > M-1, M-2, potential_2nd_max_p_nodes_2)
-            potential_2nd_max_p_nodes_1_select = np.vstack((np.arange(0, N), potential_2nd_max_p_nodes_1)).T
-            potential_2nd_max_p_nodes_2_select = np.vstack((np.arange(0, N), potential_2nd_max_p_nodes_2)).T
-            potential_2nd_max_p_1 = P.T[tuple(map(tuple, potential_2nd_max_p_nodes_1_select.T))]
-            potential_2nd_max_p_2 = P.T[tuple(map(tuple, potential_2nd_max_p_nodes_2_select.T))]
-            next_max_p_nodes = np.where(potential_2nd_max_p_1 > potential_2nd_max_p_2, potential_2nd_max_p_nodes_1, potential_2nd_max_p_nodes_2)
-            node_indices_diff = max_p_nodes - next_max_p_nodes
-            max_node_smaller_index = np.arange(0, N)[node_indices_diff < 0]
-            max_node_larger_index = np.arange(0, N)[node_indices_diff > 0]
-            dis_to_max_p_nodes = np.sqrt(np.sum(np.square(Y[max_p_nodes]-X), axis=1))
-            dis_to_2nd_largest_p_nodes = np.sqrt(np.sum(np.square(Y[next_max_p_nodes]-X), axis=1))
-            geodesic_dists = np.zeros((M, N)).T
+        # if use_geodesic:
+        #     potential_2nd_max_p_nodes_1 = max_p_nodes - 1
+        #     potential_2nd_max_p_nodes_2 = max_p_nodes + 1
+        #     potential_2nd_max_p_nodes_1 = np.where(potential_2nd_max_p_nodes_1 < 0, 1, potential_2nd_max_p_nodes_1)
+        #     potential_2nd_max_p_nodes_2 = np.where(potential_2nd_max_p_nodes_2 > M-1, M-2, potential_2nd_max_p_nodes_2)
+        #     potential_2nd_max_p_nodes_1_select = np.vstack((np.arange(0, N), potential_2nd_max_p_nodes_1)).T
+        #     potential_2nd_max_p_nodes_2_select = np.vstack((np.arange(0, N), potential_2nd_max_p_nodes_2)).T
+        #     potential_2nd_max_p_1 = P.T[tuple(map(tuple, potential_2nd_max_p_nodes_1_select.T))]
+        #     potential_2nd_max_p_2 = P.T[tuple(map(tuple, potential_2nd_max_p_nodes_2_select.T))]
+        #     next_max_p_nodes = np.where(potential_2nd_max_p_1 > potential_2nd_max_p_2, potential_2nd_max_p_nodes_1, potential_2nd_max_p_nodes_2)
+        #     node_indices_diff = max_p_nodes - next_max_p_nodes
+        #     max_node_smaller_index = np.arange(0, N)[node_indices_diff < 0]
+        #     max_node_larger_index = np.arange(0, N)[node_indices_diff > 0]
+        #     dis_to_max_p_nodes = np.sqrt(np.sum(np.square(Y[max_p_nodes]-X), axis=1))
+        #     dis_to_2nd_largest_p_nodes = np.sqrt(np.sum(np.square(Y[next_max_p_nodes]-X), axis=1))
+        #     geodesic_dists = np.zeros((M, N)).T
 
-            for idx in max_node_smaller_index:
-                geodesic_dists[idx, 0:max_p_nodes[idx]+1] = converted_node_dis[max_p_nodes[idx], 0:max_p_nodes[idx]+1] + dis_to_max_p_nodes[idx]
-                geodesic_dists[idx, next_max_p_nodes[idx]:M] = converted_node_dis[next_max_p_nodes[idx], next_max_p_nodes[idx]:M] + dis_to_2nd_largest_p_nodes[idx]
+        #     for idx in max_node_smaller_index:
+        #         geodesic_dists[idx, 0:max_p_nodes[idx]+1] = converted_node_dis[max_p_nodes[idx], 0:max_p_nodes[idx]+1] + dis_to_max_p_nodes[idx]
+        #         geodesic_dists[idx, next_max_p_nodes[idx]:M] = converted_node_dis[next_max_p_nodes[idx], next_max_p_nodes[idx]:M] + dis_to_2nd_largest_p_nodes[idx]
 
-            for idx in max_node_larger_index:
-                geodesic_dists[idx, 0:next_max_p_nodes[idx]+1] = converted_node_dis[next_max_p_nodes[idx], 0:next_max_p_nodes[idx]+1] + dis_to_2nd_largest_p_nodes[idx]
-                geodesic_dists[idx, max_p_nodes[idx]:M] = converted_node_dis[max_p_nodes[idx], max_p_nodes[idx]:M] + dis_to_max_p_nodes[idx]
+        #     for idx in max_node_larger_index:
+        #         geodesic_dists[idx, 0:next_max_p_nodes[idx]+1] = converted_node_dis[next_max_p_nodes[idx], 0:next_max_p_nodes[idx]+1] + dis_to_2nd_largest_p_nodes[idx]
+        #         geodesic_dists[idx, max_p_nodes[idx]:M] = converted_node_dis[max_p_nodes[idx], max_p_nodes[idx]:M] + dis_to_max_p_nodes[idx]
 
-            geodesic_dists = geodesic_dists.T
+        #     geodesic_dists = geodesic_dists.T
 
-            P = np.exp(-np.square(geodesic_dists) / (2 * sigma2))
+        #     P = np.exp(-np.square(geodesic_dists) / (2 * sigma2))
 
-            if (occluded_nodes is not None) and (len(occluded_nodes) != 0):
+        #     if (occluded_nodes is not None) and (len(occluded_nodes) != 0):
 
-                # modified probability distribution
-                P_vis = np.zeros((M, N))
+        #         # modified probability distribution
+        #         P_vis = np.zeros((M, N))
 
-                # determine the indices where head, tail, floating region starts/ends
-                M_head = occluded_nodes[0]
-                M_tail = M - 1 - occluded_nodes[-1]
+        #         # determine the indices where head, tail, floating region starts/ends
+        #         M_head = occluded_nodes[0]
+        #         M_tail = M - 1 - occluded_nodes[-1]
 
-                P_vis_fill_head = np.zeros((M, 1))
-                P_vis_fill_tail = np.zeros((M, 1))
-                P_vis_fill_floating = np.zeros((M, 1))
+        #         P_vis_fill_head = np.zeros((M, 1))
+        #         P_vis_fill_tail = np.zeros((M, 1))
+        #         P_vis_fill_floating = np.zeros((M, 1))
 
-                P_vis_fill_head[0 : M_head, 0] = 1 / M_head
-                P_vis_fill_tail[M-M_tail : M, 0] = 1 / M_tail
-                P_vis_fill_floating[M_head : M-M_tail, 0] = 1 / (M - M_head - M_tail)
+        #         P_vis_fill_head[0 : M_head, 0] = 1 / M_head
+        #         P_vis_fill_tail[M-M_tail : M, 0] = 1 / M_tail
+        #         P_vis_fill_floating[M_head : M-M_tail, 0] = 1 / (M - M_head - M_tail)
 
-                # fill in P_vis
-                P_vis[:, (max_p_nodes >= 0)&(max_p_nodes < M_head)] = P_vis_fill_head
-                P_vis[:, (max_p_nodes >= M-M_tail)&(max_p_nodes < M)] = P_vis_fill_tail
-                P_vis[:, (max_p_nodes >= M_head)&(max_p_nodes < M-M_tail)] = P_vis_fill_floating
-                # P_vis[:, (max_p_nodes >= 0)&(max_p_nodes <= M_head)] = P_vis_fill_head
-                # P_vis[:, (max_p_nodes >= M-M_tail-1)&(max_p_nodes < M)] = P_vis_fill_tail
-                # P_vis[:, (max_p_nodes > M_head)&(max_p_nodes < M-M_tail-1)] = P_vis_fill_floating
+        #         # fill in P_vis
+        #         P_vis[:, (max_p_nodes >= 0)&(max_p_nodes < M_head)] = P_vis_fill_head
+        #         P_vis[:, (max_p_nodes >= M-M_tail)&(max_p_nodes < M)] = P_vis_fill_tail
+        #         P_vis[:, (max_p_nodes >= M_head)&(max_p_nodes < M-M_tail)] = P_vis_fill_floating
+        #         # P_vis[:, (max_p_nodes >= 0)&(max_p_nodes <= M_head)] = P_vis_fill_head
+        #         # P_vis[:, (max_p_nodes >= M-M_tail-1)&(max_p_nodes < M)] = P_vis_fill_tail
+        #         # P_vis[:, (max_p_nodes > M_head)&(max_p_nodes < M-M_tail-1)] = P_vis_fill_floating
 
-                # modify P
-                P = P_vis * P
+        #         # modify P
+        #         P = P_vis * P
 
-                den = np.sum(P, axis=0)
-                den = np.tile(den, (M, 1))
-                den[den == 0] = np.finfo(float).eps
-                c = (2 * np.pi * sigma2) ** (D / 2) * mu / (1 - mu) / N
-                den += c
-                P = np.divide(P, den)
+        #         den = np.sum(P, axis=0)
+        #         den = np.tile(den, (M, 1))
+        #         den[den == 0] = np.finfo(float).eps
+        #         c = (2 * np.pi * sigma2) ** (D / 2) * mu / (1 - mu) / N
+        #         den += c
+        #         P = np.divide(P, den)
 
-            else:
-                den = np.sum(P, axis=0)
-                den = np.tile(den, (M, 1))
-                den[den == 0] = np.finfo(float).eps
-                c = (2 * np.pi * sigma2) ** (D / 2)
-                c = c * mu / (1 - mu)
-                c = c * M / N
-                den += c
-                P = np.divide(P, den)
+        #     else:
+        #         den = np.sum(P, axis=0)
+        #         den = np.tile(den, (M, 1))
+        #         den[den == 0] = np.finfo(float).eps
+        #         c = (2 * np.pi * sigma2) ** (D / 2)
+        #         c = c * mu / (1 - mu)
+        #         c = c * M / N
+        #         den += c
+        #         P = np.divide(P, den)
 
-            # # original method
-            # den = np.sum(P, axis=0)
-            # den = np.tile(den, (M, 1))
-            # den[den == 0] = np.finfo(float).eps
-            # c = (2 * np.pi * sigma2) ** (D / 2)
-            # c = c * mu / (1 - mu)
-            # c = c * M / N
-            # den += c
-            # P = np.divide(P, den)
+        #     # # original method
+        #     # den = np.sum(P, axis=0)
+        #     # den = np.tile(den, (M, 1))
+        #     # den[den == 0] = np.finfo(float).eps
+        #     # c = (2 * np.pi * sigma2) ** (D / 2)
+        #     # c = c * mu / (1 - mu)
+        #     # c = c * M / N
+        #     # den += c
+        #     # P = np.divide(P, den)
 
-        # if occluded_nodes is not None:
-        #     print(occluded_nodes)
-        #     P[occluded_nodes] = 0
+        # # if occluded_nodes is not None:
+        # #     print(occluded_nodes)
+        # #     P[occluded_nodes] = 0
 
         Pt1 = np.sum(P, axis=0)
         P1 = np.sum(P, axis=1)
@@ -354,21 +356,25 @@ def ecpd_lle (X_orig,                      # input point cloud
         # update Y
         if pt2pt_dis_sq(Y, Y_0 + np.matmul(G, W)) < tol:
             Y = Y_0 + np.matmul(G, W)
-            rospy.loginfo('Iteration until covnergence: ' + str(i) + '. Kernel used: ' + kernel)
+            rospy.loginfo('Iteration until covnergence: ' + str(it) + '. Kernel used: ' + kernel)
             break
         else:
             Y = Y_0 + np.matmul(G, W)
 
-        if i == max_iter - 1:
+        if it == max_iter - 1:
             # print error messages if optimization did not compile
             rospy.logerr('Optimization did not converge! ' + 'Kernel used: ' + kernel)
 
+    print("time taken:", time.time() - cur_time)
     return Y, sigma2
 
 
 if __name__=='__main__':
-    m1 = np.square(np.arange(0, 5*3, 1).reshape((5, 3))) / 10000
-    # print(m1)
+    m1 = np.arange(0, 5*3, 1).reshape((5, 3)) / 100
+    print(m1)
+
+    m2 = np.arange(0, 10*3, 1).reshape((10, 3)) / 200
+    print(m2)
 
     # # ----- test LLE weights -----
     # cur_time = time.time()
@@ -377,4 +383,4 @@ if __name__=='__main__':
     # # print(out)
 
     # test ecpd
-    print(ecpd_lle(np.array([0, 0, 0]), m1, 0.3, None, None, None, use_geodesic=False, kernel="Gaussian"))
+    print(ecpd_lle(m2, m1, 0.3, 1, 1, 0.1, max_iter=30, include_lle=True, use_geodesic=False, kernel="Gaussian"))
