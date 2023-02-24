34172b082d5e59ca5bf13a4f59051a98485aeda8
JeffersonAero <holly.dinkel@gmail.com>
2023-02-24 17:22:16 -0600

    change to plotting from .txt files

--- utils/plot.py
+++ utils/plot.py
@@ -1,32 +1,33 @@
-import json
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import numpy as np
-from labellines import labelLine, labelLines
+from labellines import labelLines
+
 # , 'gltp', 'cdcpd'
-algorithms = ['trackdlo', 'gltp', 'cdcpd']
+algorithms = ['trackdlo', 'gltp', 'cdcpd2']
+bags = ['stationary']
 algorithms_plot = {'trackdlo': 'TrackDLO',
                     'gltp': 'GLTP',
-                    'cdcpd': 'CDCPD'}
-pct_occlusion = [0,25,50,75]
+                    'cdcpd': 'CDCPD',
+                    'cdcpd2': 'CDCPD2'}
 colors = ['g','b','c','r']
-window_size = 100
+window_size = 10
 ROOT_DIR = os.path.abspath(os.curdir)
-idx=0
-ax = plt.gca()
-for occ in pct_occlusion:
-    for i, algorithm in enumerate(algorithms):
-        for trial in range(1,11):
-            files = os.listdir(f'{ROOT_DIR}/src/trackdlo/data/output/{algorithm}')
-            data_list = []
-            for file in files:
-                f = open(f'{ROOT_DIR}/src/trackdlo/data/output/{algorithm}/frame_error_eval_{algorithm}_{trial}_{occ}.json')
-                data = json.load(f)
-                data_list.append(data['error'])
 
-        mean_data_array = np.asarray(data_list).mean(axis=0)*1000
-        std_data_array = np.asarray(data_list).std(axis=0)*1000
+ax = plt.gca()
+for i, algorithm in enumerate(algorithms):
+    for bag in bags:
+        time = []
+        error = []
+        with open(f'{ROOT_DIR}/data/dlo_tracking_eval/{algorithm}_{bag}_error.txt', 'r') as file:
+            content = file.readlines()
+            for line in content:
+                row = line.split()
+                time.append(float(row[0]))
+                error.append(float(row[1]))
+
+        mean_data_array = np.asarray(error)*1000
 
         average_smoothed_error = pd.Series(list(mean_data_array)).rolling(window_size).mean().tail(-window_size)
         std_smoothed_error = pd.Series(list(mean_data_array)).rolling(window_size).std().tail(-window_size)
@@ -35,10 +36,10 @@ for occ in pct_occlusion:
         ax.plot(average_smoothed_error, label=f'{algorithms_plot[algorithm]}', alpha=1.0, color=colors[i])
         ax.fill_between(average_smoothed_error.index, (average_smoothed_error - std_smoothed_error), (average_smoothed_error + std_smoothed_error), alpha=0.2, color=colors[i])
 
-    labelLines(ax.get_lines(), align=False, zorder=2.5)
-    plt.title('Stationary Rope')
-    plt.xlabel('Frame Index')
-    plt.ylabel('Frame Error (mm)')
-    # plt.legend(framealpha=1, frameon=True)
-    plt.savefig(f'{ROOT_DIR}/src/trackdlo/data/output/frame_error_eval_{occ}.png')
-    plt.cla()
+labelLines(ax.get_lines(), align=False, zorder=2.5)
+plt.title('Stationary Rope')
+plt.xlabel('Frame Index')
+plt.ylabel('Frame Error (mm)')
+# plt.legend(framealpha=1, frameon=True)
+plt.savefig(f'{ROOT_DIR}/data/frame_error_eval_{bag}.png')
+# plt.cla()
