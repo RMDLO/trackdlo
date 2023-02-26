4c7097582202b3c5125102447790fb8500ff546f
JeffersonAero <holly.dinkel@gmail.com>
2023-02-25 19:58:33 -0600

    update save loc and rate in evaluaion.launch, num nodes in gltp.launch+trackdlo.launch+, all bash scripts, plot script

--- utils/evaluation_cdcpd2.sh
+++ utils/evaluation_cdcpd2.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 # Loop
 
-# make directories
+# for bag in 0
+# do
+#     for pct in 0 25 50
+#     do
+#         for trial in 1 2 3 4 5 6 7 8 9
+#         do
+#             for alg in cdcpd2
+#             do
+#                 terminator -e "cd rmdlo_tracking && source devel/setup.bash && roslaunch cdcpd_ros cdcpd2.launch bag_file:=$bag" &
+#                 first_teminal=$!
+#                 terminator -e "cd rmdlo_tracking && source devel/setup.bash && roslaunch trackdlo evaluation.launch alg:=$alg bag_file:=$bag trial:=$trial pct_occlusion:=$pct --wait" &
+#                 second_teminal=$!
+#                 sleep 80
+#                 terminator -e "rosnode kill -a && killall -9 rosmaster"
+#                 # killall -9 rosmaster
+#                 kill $first_terminal
+#                 kill $second_terminal
+#             done
+#         done
+#     done
+# done
 
-for bag in stationary.bag
+for bag in 1 2
 do
-    for pct in 0 25 50 75
+    for pct in 0
     do
-        for trial in 1 2 3 4 5 6 7 8 9 10
+        for trial in 0 1 2 3 4 5 6 7 8 9
         do
             for alg in cdcpd2
             do
-                echo "starting roscore"
-                terminator -e 'roscore -p 1234' &
+                terminator -e "cd rmdlo_tracking && source devel/setup.bash && roslaunch cdcpd cdcpd2.launch bag_file:=$bag" &
                 first_teminal=$!
-                echo "starting nodes for $alg evaluation" &
-                terminator -e "source ~/.bashrc && cd rmdlo_tracking/ && source devel/setup.bash && rosrun trackdlo trackdlo" &
+                terminator -e "cd rmdlo_tracking && source devel/setup.bash && roslaunch trackdlo evaluation.launch alg:=$alg bag_file:=$bag trial:=$trial pct_occlusion:=$pct --wait" &
                 second_teminal=$!
-                terminator -e "source ~/.bashrc && cd rmdlo_tracking/ && source devel/setup.bash && rosrun trackdlo evaluation.py $trial $pct $alg $bag" &
-                third_teminal=$!
-                terminator -e "source ~/.bashrc && cd rmdlo_tracking/src/trackdlo/data/bags && rosbag play -r 0.05 $bag" &
-                fourth_teminal=$!
-                sleep 1000
+                sleep 80
                 rosnode kill -a
                 killall -9 rosmaster
                 kill $first_terminal
                 kill $second_terminal
-                kill $third_terminal
-                kill $fourth_terminal
             done
         done
     done
 done
-
-terminator -e "python rmdlo_tracking/src/trackdlo/utils/plot.py" 
-last_teminal=$!
-sleep 2
-kill $last_terminal
